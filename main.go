54f40589fe222b9fe6251f56486a1fc2ed1b8d4e
piglet <2792904471@qq.com>
2025-12-23 09:49:15 +0800

    chapter1

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strings"
+)
+
+func main() {
+	fmt.Println(math.Floor(2.75))
+	fmt.Println(strings.ToUpper("hello, world!"))
+	fmt.Print('覃')
+	fmt.Print(true)
+}
---
+++ variable.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var quantity int
+	quantity = 42
+	fmt.Println(quantity)
+
+	var prince = 99.99
+	fmt.Println(prince)
+
+	isAvailable := true
+	fmt.Println(isAvailable)
+
+	var name string = "Gopher"
+	fmt.Println(name)
+}
