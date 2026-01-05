a440c24fa7191bae1c7b925aa3af9e4c0ded68bc
piglet <2792904471@qq.com>
2026-01-05 19:29:36 +0800

    260105

---
+++ guess.go
@@ -0,0 +1,52 @@
+// A simple number guessing game in Go.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	target := rand.Intn(100) + 1
+	fmt.Println("I've chosen a random number between 1 and 100.")
+	fmt.Println("Can you guess it?")
+
+	reader := bufio.NewReader(os.Stdin)
+
+	success := false
+	for guesses := 0; guesses < 10; guesses++ {
+		fmt.Println("You have", 10-guesses, "guesses left.")
+		fmt.Print("Make a guess: ")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		input = strings.TrimSpace(input)
+		guess, err := strconv.Atoi(input)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if guess < target {
+			fmt.Println("Too low!")
+		} else if guess > target {
+			fmt.Println("Too high!")
+		} else {
+			success = true
+			fmt.Println("You got it!")
+			break
+		}
+	}
+
+	if !success {
+		fmt.Println("Sorry, the number was", target)
+	}
+}
